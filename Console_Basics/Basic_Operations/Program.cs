b5274708191d35bd4a8c994fef95d9d7b2c38cff
SaiDurga <saiprasadveluri@gmail.com>
2025-08-01 06:16:00 +0000

    initial commit

---
+++ Console_Basics/Basic_Operations/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       // 1.
+        Console.Write("Enter first number to add: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number to add: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine("Sum = " + sum);
+
+        Console.Write("Enter first number to multiply : ");
+        int m1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter second number to multiply : ");
+        int m2 = int.Parse(Console.ReadLine());
+        int product = m1 * m2;
+        Console.WriteLine("Product = " + product);
+
+        //2.
+
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        if (age >= 18)
+        {
+            Console.WriteLine("You are eligible to vote ");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible to vote ");
+        }
+
+        Console.Write("Enter a number to check value :  ");
+        int checkNum = int.Parse(Console.ReadLine());
+        if (checkNum > 0)
+        {
+            Console.WriteLine("Number is Positive");
+        }
+        else if (checkNum < 0)
+        {
+            Console.WriteLine("Number is Negative");
+        }
+        else
+        {
+            Console.WriteLine("Number is Zero");
+        }
+
+        Console.Write("Enter a number to check even or odd : ");
+        int evenOddNum = int.Parse(Console.ReadLine());
+        if (evenOddNum % 2 == 0)
+        {
+            Console.WriteLine("Number is Even");
+        }
+        else
+        {
+            Console.WriteLine("Number is Odd");
+        }
+
+        // 3
+        Console.WriteLine("Numbers from 1 to 10 , using loopings ");
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+        int totalSum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            totalSum += i;
+        } 
+        Console.WriteLine("Sum from 1 to 100  = " + totalSum);
+
+        Console.WriteLine("Even numbers between 1 and 20 : ");
+
+        int j = 2;
+        while (j <= 20)
+        {
+            Console.Write(j + " ");
+            j += 2;
+        }
+
+    }
+}
---
+++ Console_Basics/Evaluation/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+
+    public static void Main()
+    {
+        //DateTime dobj = DateTime.Now;
+        ////Console.Write("obj = "+dobj.AddMinutes(90) );
+
+        //StringBuilder s = new StringBuilder(Console.ReadLine());
+        //s.Append("Hi");
+
+        //Console.WriteLine(s);
+
+        Student student = new Student();
+        Student obj2 = new Student(18);
+
+        //string firstName = Console.ReadLine();
+        //string lastName = Console.ReadLine();
+
+        student.firstName = "Ey";
+        student.lastName = " Gds";
+
+        //student.Display(firstName, lastName);
+
+        int[] arr = new int[5];
+
+        arr[0] = 100; arr[1] = 200; arr[2] = 300; arr[3] = 400; arr[4] = 500;
+
+        int[] arr2 = new int[] { 100, 200, 300, 400, 500 };
+
+        for (int i = 0; i < arr2.Length; i++) { 
+            Console.Write(arr2[i]);
+        }
+        Console.WriteLine();
+
+        foreach(int i in arr)
+        {
+            Console.Write(i);
+        }
+        //Base class Animal -> speak()
+        //Derived Dog->override speak()
+
+        //Dog animal = new Animal();
+
+        Animal a = new Dog();
+
+        a.speak();
+
+        
+
+
+
+    }
+}
+
+class Student
+{
+    readonly int maxCount = 100;
+    const int maxSubjects = 10;
+
+
+    public Student() {
+
+        maxCount = 200;
+        //maxSubjects = 30;
+
+    }
+    public Student(int age )
+    {
+        Console.WriteLine("age = " + age);
+    }
+
+    private string first_Name;
+    private string last_Name;
+
+    public int age { get; set; }
+
+ 
+    public string firstName { 
+        get 
+            { return first_Name; }
+        set
+        {
+            first_Name = value;
+        }
+    }
+
+    public string lastName { 
+        get {
+            return last_Name;
+        }
+        set
+        {
+            last_Name = value;
+        }
+    }
+
+    public void Display(string fName, string lName)
+    {
+        firstName = fName;
+        lastName = lName;
+
+        Console.WriteLine("First name : " + first_Name + ". Last name : " + last_Name);
+    }
+
+    public int Addition(int x , int y)
+    {
+        int sum = x + y;
+        //Console.WriteLine("Sum = " + sum);
+        return sum;
+    }
+
+    public bool ageCheck(int Age)
+    {
+        age = Age;
+        if (age > 18) return true;
+
+        return false;
+
+    }
+
+
+
+
+}
+
+
+class Animal
+{
+    public virtual void speak()
+    {
+        Console.WriteLine("Animal speak");
+    }
+}
+
+class Dog : Animal
+{
+    public override void speak()
+    {
+        //base.speak()
+        Console.WriteLine("Dog is speaking");
+    }
+}
