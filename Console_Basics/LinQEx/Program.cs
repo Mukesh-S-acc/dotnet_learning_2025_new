bd7583b75e0148d146ed13a492c69ff11d927bcf
SaiDurga <saiprasadveluri@gmail.com>
2025-08-12 04:25:14 +0000

    LinQ ex 2

---
+++ Console_Basics/LinQEx/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Employee
+{
+    public int EmpId { get; set; }
+    public string EmpName { get; set; }
+    public string City { get; set; }
+    public int Salary { get; set; }
+    public int DeptNo { get; set; }
+}
+
+class Program
+{
+
+    public static void Main()
+    {
+
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee { EmpId = 1, EmpName = "John", City = "Pune", Salary = 20000, DeptNo = 10 },
+            new Employee { EmpId = 2, EmpName = "Sara", City = "Mumbai", Salary = 30000, DeptNo = 20 },
+            new Employee { EmpId = 3, EmpName = "Kia", City = "Pune", Salary = 25000, DeptNo = 10 },
+            new Employee { EmpId = 4, EmpName = "Raj", City = "Delhi", Salary = 40000, DeptNo = 30 },
+            new Employee { EmpId = 5, EmpName = "Simran", City = "Pune", Salary = 35000, DeptNo = 20 },
+            new Employee { EmpId = 6, EmpName = "Amit", City = "Mumbai", Salary = 15000, DeptNo = 20 }
+       };
+
+
+        var ids = employees.Select(e => e.EmpId).ToList();
+        //Console.WriteLine(ids);
+        //foreach(var i in ids)
+        //{
+        //    Console.WriteLine(i+ " ");
+        //}
+        var dept10 = employees.Where(e => e.DeptNo == 10);
+        //foreach (var i in dept10)
+        //{
+        //    Console.WriteLine(i.EmpName + " ");
+        //}
+        var firstEmp = employees.First();
+        //Console.WriteLine(firstEmp.EmpName);
+        bool anyDelhi = employees.Any(e => e.City == "Delhi");
+        bool allAbove = employees.All(e => e.Salary > 10000);
+        int totalCount = employees.Count();
+        var startWithS = employees.Where(e => e.EmpName.StartsWith("S"));
+        //foreach (var i in startWithS)
+        //{
+        //    Console.WriteLine(i.EmpName + " ");
+        //}
+
+
+        var orderedBySalary = employees.OrderByDescending(e => e.Salary);
+        //foreach (var i in orderedBySalary)
+        //{
+        //    Console.WriteLine(i.EmpName + " ");
+        //}
+
+        var top2 = employees.OrderByDescending(e => e.Salary).Take(2);
+        var skip = employees.Skip(2);
+        //foreach (var i in skip)
+        //{
+        //    Console.WriteLine(i.EmpName + " ");
+        //}
+        var avgSalary = employees.Average(e => e.Salary);
+        var deptSalary = employees.Where(e => e.DeptNo == 20).Sum(e => e.Salary);
+        var minSalary = employees.OrderBy(e => e.Salary).First();
+        var Uppername = employees.Select(e => e.EmpName.ToUpper());
+        //foreach (var i in Uppername)
+        //{
+        //    Console.WriteLine(i + " ");
+        //}
+
+
+        var highinpune = employees.Where(e => e.City == "Pune" && e.Salary > 25000);
+        var groupByCity = employees.GroupBy(e => e.City)
+            .Select(g => new { City = g.Key, Count = g.Count() });
+
+        var mumbaiNames = employees.Where(e => e.City == "Mumbai")
+            .OrderBy(e => e.EmpName)
+            .Select(e => e.EmpName);
+
+        //foreach (var i in mumbaiNames)
+        //{
+        //    Console.WriteLine(i + " ");
+        //}
+        var namestore = employees.Select(e=>e.EmpName);
+        var joinString = string.Join(",", namestore);
+        Console.WriteLine(joinString);
+
+        var rangeSalary = employees
+            .Where(e => e.Salary >= 20000 && e.Salary <= 35000)
+            .Select(e => e.EmpName);
+
+        bool EmpExists = employees.Any(e => e.EmpId == 4);
+        var first3Alpha = employees.OrderBy(e => e.EmpName).Take(3).Select(e => e.EmpName);
+
+    }
+}
--- Console_Basics/LinqExercise/Program.cs
+++ Console_Basics/LinqExercise/Program.cs
@@ -22,96 +22,98 @@ class Program
         
             {
                 new Employee { Empid=1, EmpName="Sara", City="Pune", Salary=20000, Deptno=10 },
+
                 new Employee { Empid=2, EmpName="Piya", City="Chennai", Salary=30000, Deptno=20 },
                 new Employee { Empid=3, EmpName="Kia", City="Bangalore", Salary=5000, Deptno=30 },
-                new Employee { Empid=4, EmpName="John", City="Pune", Salary=0, Deptno=10 },
-                new Employee { Empid=5, EmpName="Anna", City="Coimbatore", Salary=0, Deptno=10 },
-                new Employee { Empid=6, EmpName="Mike", City="Pune", Salary=0, Deptno=10 },
+                new Employee { Empid=4, EmpName="John", City="Pune", Salary=10, Deptno=10 },
+                new Employee { Empid=5, EmpName="Anna", City="Coimbatore", Salary=10, Deptno=10 },
+                new Employee { Empid=6, EmpName="Mike", City="Pune", Salary=10, Deptno=10 },
+                new Employee { Empid=7, EmpName="Ravi", City="Pune", Salary=10000, Deptno=10 },
          };
 
-    //    var cityGroup = employees.GroupBy(e=>e.City).Select(g=>new {City = g.Key,EmployeeCount = g.Count()});
+        var cityGroup = employees.GroupBy(e => e.City).Select(g => new { City = g.Key, EmployeeCount = g.Count() });
 
-    //    foreach (var cg in cityGroup)
-    //    {
-    //        Console.WriteLine($"City: {cg.City}, Total Employees: {cg.EmployeeCount}");
-    //    }
-
-
+        foreach (var cg in cityGroup)
+        {
+            Console.WriteLine($"City: {cg.City}, Total Employees: {cg.EmployeeCount}");
+        }
 
 
-    //    var highestInDept = employees.GroupBy(e => e.Deptno).Select(g => new{
-    //    DeptNo = g.Key,
-    //    MaxSalary = g.Max(e => e.Salary),
-    //    TopEarners = g.Where(e => e.Salary == g.Max(emp => emp.Salary))});
-
-    //    foreach (var dept in highestInDept)
-    //    {
-    //        Console.WriteLine($"Dept: {dept.DeptNo}, Highest Salary: {dept.MaxSalary}");
-    //        foreach (var emp in dept.TopEarners)
-    //        { Console.WriteLine($" {emp.EmpName}"); }
-    //    }
 
 
-    //    var highSalary = employees.Where(e => e.Salary > 20000).Select(e => new { e.EmpName, e.City });
+        //    var highestInDept = employees.GroupBy(e => e.Deptno).Select(g => new{
+        //    DeptNo = g.Key,
+        //    MaxSalary = g.Max(e => e.Salary),
+        //    TopEarners = g.Where(e => e.Salary == g.Max(emp => emp.Salary))});
 
-    //    foreach (var i in highSalary)
-    //    {
-    //        Console.WriteLine($"Name: {i.EmpName}, City : {i.City}");
-    //    }
-
-
-    //    var anObj = employees.Select(e => new { e.EmpName,e.City});
-
-    //    foreach (var o in anObj)
-    //    {
-    //        Console.WriteLine($"Name: {o.EmpName}, city: {o.City}");
-    //    }
+        //    foreach (var dept in highestInDept)
+        //    {
+        //        Console.WriteLine($"Dept: {dept.DeptNo}, Highest Salary: {dept.MaxSalary}");
+        //        foreach (var emp in dept.TopEarners)
+        //        { Console.WriteLine($" {emp.EmpName}"); }
+        //    }
 
 
-    //    var groupByDept = employees.GroupBy(e => e.Deptno).Select(g => new{
-    //    DeptNo = g.Key,
-    //    Count = g.Count(),
-    //    AvgSalary = g.Average(e => e.Salary)
-    //    });
+        var highSalary = employees.Where(e => e.Salary > 20000).Select(e => new { e.EmpName, e.City });
 
-    //    foreach (var dept in groupByDept)
-    //    { Console.WriteLine($"Dept: {dept.DeptNo}, Count: {dept.Count}, Avg Salary: {dept.AvgSalary}"); }
+        foreach (var i in highSalary)
+        {
+            Console.WriteLine($"Name: {i.EmpName}, City : {i.City}");
+        }
 
-    //    var empByCity = employees.GroupBy(e => e.City).Select(a => new
-    //    {
-    //        City = a.Key,
-    //        TotEmp = a.Count(),
-    //        Salaries = a.Select(e=>e.Salary).ToList()
-    //    });
 
-    //    Console.WriteLine();
+        //    var anObj = employees.Select(e => new { e.EmpName,e.City});
 
-    //    foreach (var city in empByCity)
-    //    {
-    //        Console.Write($"City: {city.City}, Total Employees: {city.TotEmp}");
-    //        Console.Write("Salaries: ");
+        //    foreach (var o in anObj)
+        //    {
+        //        Console.WriteLine($"Name: {o.EmpName}, city: {o.City}");
+        //    }
 
-    //        foreach (var salary in city.Salaries)
-    //        {
-    //            Console.Write(salary);
-    //        }
-    //        Console.ReadLine();
-    //    }
 
-    //    var avgSalaryPerDept = employees.Where(e => e.Salary > 0).GroupBy(e => e.Deptno).Select(g => new
-    //{
-    //    DeptNo = g.Key,
-    //    AvgSalary = g.Average(e => e.Salary)
-    //});
+        //    var groupByDept = employees.GroupBy(e => e.Deptno).Select(g => new{
+        //    DeptNo = g.Key,
+        //    Count = g.Count(),
+        //    AvgSalary = g.Average(e => e.Salary)
+        //    });
 
-    //    foreach (var dept in avgSalaryPerDept)
-    //    { Console.WriteLine($"Dept: {dept.DeptNo},Avg Salary: {dept.AvgSalary}"); }
+        //    foreach (var dept in groupByDept)
+        //    { Console.WriteLine($"Dept: {dept.DeptNo}, Count: {dept.Count}, Avg Salary: {dept.AvgSalary}"); }
+
+        //    var empByCity = employees.GroupBy(e => e.City).Select(a => new
+        //    {
+        //        City = a.Key,
+        //        TotEmp = a.Count(),
+        //        Salaries = a.Select(e=>e.Salary).ToList()
+        //    });
+
+        //    Console.WriteLine();
+
+        //    foreach (var city in empByCity)
+        //    {
+        //        Console.Write($"City: {city.City}, Total Employees: {city.TotEmp}");
+        //        Console.Write("Salaries: ");
+
+        //        foreach (var salary in city.Salaries)
+        //        {
+        //            Console.Write(salary);
+        //        }
+        //        Console.ReadLine();
+        //    }
+
+        //    var avgSalaryPerDept = employees.Where(e => e.Salary > 0).GroupBy(e => e.Deptno).Select(g => new
+        //{
+        //    DeptNo = g.Key,
+        //    AvgSalary = g.Average(e => e.Salary)
+        //});
+
+        //    foreach (var dept in avgSalaryPerDept)
+        //    { Console.WriteLine($"Dept: {dept.DeptNo},Avg Salary: {dept.AvgSalary}"); }
 
 
         var nonZeroSalaryGroups = employees.Where(e => e.Salary > 0).GroupBy(e => e.Deptno).Select(g => new
         {
-        DeptNo = g.Key,
-        EmployeeNames = g.Select(e => e.EmpName)
+            DeptNo = g.Key,
+            EmployeeNames = g.Select(e => e.EmpName).ToList()
         });
 
         foreach (var dept in nonZeroSalaryGroups)
@@ -121,7 +123,7 @@ class Program
             Console.Write(" Employees:");
             foreach(var i in dept.EmployeeNames)
             {
-                Console.Write(i + ", ");
+                Console.Write(i + ", "); 
             }
             Console.WriteLine();
         }
